3f28b8a5c35ff13e9f53e494763ae79e284db25c
Tigran Matsakyan <matsakyan.7@gmail.com>
2019-04-24 18:39:34 +0400

    create simple cli

--- createExpressApp.js
+++ createExpressApp.js
@@ -1,7 +1,20 @@
-const programm = require('commander');
+const { Command } = require('commander');
+const { green, red } = require('chalk');
 
 const packageJSON = require('./package.json');
 
-programm
+let name;
+
+const programm = new Command(green(packageJSON.name))
   .version(packageJSON.version, '-v, --version')
+  .usage(green('<project-name> [options]'))
+  .arguments('<project-name>')
+  .action(n => name = n)
   .parse(process.argv)
+
+if (!name) {
+  console.log(red('Please specify project name'));
+  console.log();
+  console.log(`Run ${green('create-react-app --help')} to see all options.`);
+  process.exit(1);
+}
