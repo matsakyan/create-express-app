bfc488c4c70d05bcbd76769848560459f9482f61
Tigran Matsakyan <matsakyan.7@gmail.com>
2019-04-24 22:58:25 +0400

    add project name check

--- createExpressApp.js
+++ createExpressApp.js
@@ -1,20 +1,41 @@
 const { Command } = require('commander');
 const { green, red } = require('chalk');
+const validateName = require('validate-npm-package-name');
 
 const packageJSON = require('./package.json');
 
-let name;
+let projectName;
 
 const programm = new Command(green(packageJSON.name))
   .version(packageJSON.version, '-v, --version')
   .usage(green('<project-name> [options]'))
   .arguments('<project-name>')
-  .action(n => name = n)
+  .action(name => projectName = name)
   .parse(process.argv)
 
-if (!name) {
-  console.log(red('Please specify project name'));
-  console.log();
-  console.log(`Run ${green('create-react-app --help')} to see all options.`);
-  process.exit(1);
+checkName(projectName);
+
+function checkName(name) {
+  // TODO ::: Project can't have same name as it's dependencies
+  if (!name) {
+    console.log(red('Please specify project name'));
+    console.log();
+    console.log(`Run ${green('create-react-app --help')} to see all options.`);
+    process.exit(1);
+  }
+
+  const {
+    validForNewPackages,
+    errors,
+    warnings,
+  } = validateName(name);
+
+  if (!validForNewPackages) {
+    console.log(`${red(name)} is not a valid npm package name.`);
+    errors && console.log();
+    errors && console.log(red(errors.join('\n')));
+    warnings && console.log();
+    warnings && console.log(red(warnings.join('\n')));
+    process.exit(1);
+  }
 }
