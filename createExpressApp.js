fa9e2954d2912d00612982d92fdad4d8bfb3b9a2
Tigran Matsakyan <matsakyan.7@gmail.com>
2019-04-25 13:44:40 +0400

    create package.json with deps

--- createExpressApp.js
+++ createExpressApp.js
@@ -1,19 +1,54 @@
 const { Command } = require('commander');
 const { green, red } = require('chalk');
 const validateName = require('validate-npm-package-name');
+const path = require('path');
+const fs = require('fs-extra');
+const spawn = require('cross-spawn');
 
 const packageJSON = require('./package.json');
 
+const deps = ['express', 'nconf'];
+const devDeps = ['nodemon', 'cors'];
+
 let projectName;
 
-const programm = new Command(green(packageJSON.name))
+new Command(green(packageJSON.name))
   .version(packageJSON.version, '-v, --version')
   .usage(green('<project-name> [options]'))
   .arguments('<project-name>')
   .action(name => projectName = name)
-  .parse(process.argv)
+  .parse(process.argv);
 
-checkName(projectName);
+start(projectName);
+
+function start(name) {
+  checkName(name);
+
+  const root = path.resolve(name);
+
+  fs.ensureDirSync(root);
+
+  console.log(`Creating a new Express app in ${green(root)}`);
+  console.log();
+
+  const packageJSON = {
+    name,
+    version: '1.0.0',
+    private: true,
+    description: "",
+    main: "index.js",
+  }
+
+  fs.writeFileSync(
+    path.join(root, 'package.json'),
+    JSON.stringify(packageJSON, null, 2),
+  );
+
+  process.chdir(root);
+
+  console.log('Installing dependencies...');
+  npmInstall();
+}
 
 function checkName(name) {
   // TODO ::: Project can't have same name as it's dependencies
@@ -38,4 +73,10 @@ function checkName(name) {
     warnings && console.log(red(warnings.join('\n')));
     process.exit(1);
   }
+}
+
+function npmInstall() {
+  // TODO ::: check exit code
+  spawn('npm', ['install'].concat(deps), { stdio: 'inherit' });
+  spawn('npm', ['install', '--save-dev'].concat(devDeps), { stdio: 'inherit' });
 }
